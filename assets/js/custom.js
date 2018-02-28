d15d323abc11fa5183d43a7ac9300324b335064c
Johnathan Zhou <johnathanz@berkeley.edu>
2018-02-28 15:05:22 -0800

    upload from original source

---
+++ assets/js/custom.js
@@ -0,0 +1,10 @@
+
+(function($) {
+    "use strict";
+    $(document).on("ready", function () {
+      particlesJS.load('particles-js', '../particles.json', function() {
+        console.log('callback - particles.js config loaded');
+        });
+    })
+
+})(jQuery);
